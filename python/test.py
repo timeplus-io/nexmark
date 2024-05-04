8422837e2966d5fb40f89b44de624485997618c6
Gang Tao <gang@timeplus.io>
2024-05-04 10:44:06 -0700

    update reources config

--- python/test.py
+++ python/test.py
@@ -35,7 +35,7 @@ def start_kafka():
             '--rpc-addr', 'kafka:33145',
             '--advertise-rpc-addr', 'kafka:33145',
             '--smp', '1',
-            '--memory', '8G',
+            '--memory', '1G',
             '--mode', 'dev-container',
             '--default-log-level=debug'
         ],
@@ -169,7 +169,7 @@ def start_flink():
         'environment': [
             'FLINK_PROPERTIES=jobmanager.rpc.address: flink-jobmanager\ntaskmanager.numberOfTaskSlots: 1\ntaskmanager.memory.flink.size: 3g\ntaskmanager.memory.process.size: 4g'
         ],
-        'mem_limit': '16g',
+        'mem_limit': '4g',
         'cpu_period': cpu_period,
         'cpu_quota':cpu_quota,
         'network': network_name,
@@ -233,7 +233,7 @@ def start_proton():
             '8463/tcp':8463 # TCP Streaming
         },
         'name': 'proton',
-        'mem_limit': '16g',
+        'mem_limit': '4g',
         'cpu_period': cpu_period,
         'cpu_quota':cpu_quota,
         'network': network_name,
@@ -278,7 +278,7 @@ def start_ksqldb():
         'image': 'confluentinc/ksqldb-server:0.29.0',
         'ports': {'8088/tcp': 8088},
         'name': 'ksqldb',
-        'mem_limit': '16g',
+        'mem_limit': '4g',
         'cpu_period': cpu_period,
         'cpu_quota':cpu_quota,
         'network': network_name,
