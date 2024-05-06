1a7a394f892c4a5ed52b110a0f4314412b4f624c
Gang Tao <gang@timeplus.io>
2024-05-06 16:35:11 -0700

    update q4

--- python/scripts/ksqldb/q4.sql
+++ python/scripts/ksqldb/q4.sql
@@ -1,11 +1,50 @@
-CREATE STREAM q4_stream (
-    id INT,
-    message VARCHAR
+CREATE STREAM auction (
+  id INT,
+  item_name VARCHAR,
+  description VARCHAR,
+  initial_bid INT,
+  reserve INT,
+  date_time VARCHAR,
+  expires  VARCHAR,
+  seller INT,
+  category INT,
+  extra VARCHAR
 ) WITH (
-    kafka_topic='NEXMARK_Q4', 
-    value_format='json'
+    KAFKA_TOPIC='nexmark-auction',
+    VALUE_FORMAT='JSON'
 );
 
-INSERT INTO q4_stream (id, message)
-VALUES
-    (1, 'query q4 does not work!');
+CREATE STREAM bid
+(
+  auction  INT,
+  bidder  INT,
+  price  INT,
+  channel  VARCHAR,
+  url  VARCHAR,
+  date_time  VARCHAR,
+  extra  VARCHAR
+) WITH (
+    KAFKA_TOPIC='nexmark-bid',
+    VALUE_FORMAT='JSON'
+);
+
+SET 'auto.offset.reset' = 'earliest';
+
+CREATE TABLE max_table
+    WITH (FORMAT='JSON')
+    AS
+    SELECT MAX(B.price) AS final_bid, A.category AS auction_category, A.id as auction_id
+        FROM auction A
+            INNER JOIN bid B WITHIN 2 HOURS GRACE PERIOD 1 SECONDS
+            ON A.id = B.auction
+        WHERE B.date_time BETWEEN A.date_time AND A.expires
+        GROUP BY A.id, A.category
+        EMIT CHANGES;
+
+CREATE TABLE output_stream
+    WITH (kafka_topic='NEXMARK_Q4', key_format='json', value_format='json')
+    AS SELECT
+        Q.auction_category,
+        AVG(Q.final_bid)
+    FROM max_table as Q
+    GROUP BY Q.auction_category;
