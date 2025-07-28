8aaa9f4677e1049ee47c9cdde7de9ede4de5f1c2
kui.liu <165614821+kuiliu88@users.noreply.github.com>
2025-07-28 16:25:16 +0800

    Update q16.sql

--- python/scripts/timeplus/q16.sql
+++ python/scripts/timeplus/q16.sql
@@ -1,4 +1,4 @@
-CREATE STREAM bid
+CREATE STREAM bid_ext
 (
   auction  int64,
   bidder  int64,
@@ -32,7 +32,30 @@ CREATE EXTERNAL STREAM target(
              topic='NEXMARK_Q16', 
              data_format='JSONEachRow',
              one_message_per_row=true;
-
+CREATE STREAM bid
+(
+  auction  int64,
+  bidder  int64,
+  price  int64,
+  channel  string,
+  url  string,
+  date_time  datetime64,
+  extra  string
+);
+select sleep(3);
+CREATE MATERIALIZED VIEW sink_mv INTO bid AS
+    select
+        raw:auction::int64 AS auction,
+        raw:bidder::int64 AS bidder,
+        raw:price::int64 AS price,
+        raw:channel::string AS channel,
+        raw:url::string AS url,
+        raw:date_time:datetime64 AS date_time,
+        raw:extra AS extra
+    FROM bid_ext
+    SETTINGS seek_to = 'earliest';
+             data_format='JSONEachRow',
+             one_message_per_row=true;
 -- 
 CREATE MATERIALIZED VIEW mv INTO target AS 
   SELECT
