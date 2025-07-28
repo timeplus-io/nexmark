10830c3947602d41f2a46531d33de513b7786697
kui.liu <165614821+kuiliu88@users.noreply.github.com>
2025-07-28 17:05:27 +0800

    Update q20.sql

--- python/scripts/timeplus/q20.sql
+++ python/scripts/timeplus/q20.sql
@@ -1,3 +1,11 @@
+drop stream if exists sink_mv;
+drop stream if exists mv;
+select sleep(3);
+drop stream if exists bid;
+drop stream if exists target;
+drop stream if exists bid_ext;
+drop stream if exists auction;
+
 CREATE STREAM auction
 (
   id int64,
@@ -14,7 +22,7 @@ CREATE STREAM auction
 ENGINE = ExternalStream
 SETTINGS type = 'kafka', brokers = 'kafka:9092', topic = 'nexmark-auction', properties='queued.min.messages=10000000;queued.max.messages.kbytes=655360';
 
-CREATE STREAM bid
+CREATE STREAM bid_ext
 (
   auction  int64,
   bidder  int64,
@@ -47,6 +55,30 @@ CREATE EXTERNAL STREAM target(
     SETTINGS type='kafka', 
              brokers='kafka:9092', 
              topic='NEXMARK_Q20', 
+             data_format='JSONEachRow',
+             one_message_per_row=true;
+CREATE STREAM bid
+(
+  auction  int64,
+  bidder  int64,
+  price  int64,
+  channel  string,
+  url  string,
+  date_time  datetime64,
+  extra  string
+);
+select sleep(3);
+CREATE MATERIALIZED VIEW sink_mv INTO bid AS
+    select
+        raw:auction::int64 AS auction,
+        raw:bidder::int64 AS bidder,
+        raw:price::int64 AS price,
+        raw:channel::string AS channel,
+        raw:url::string AS url,
+        raw:date_time:datetime64 AS date_time,
+        raw:extra AS extra
+    FROM bid_ext
+    SETTINGS seek_to = 'earliest';
              data_format='JSONEachRow',
              one_message_per_row=true;
 
