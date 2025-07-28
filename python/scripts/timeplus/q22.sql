3711ba0154a05b9eb0509a8ee50cf274de9cacd7
kui.liu <165614821+kuiliu88@users.noreply.github.com>
2025-07-28 17:07:33 +0800

    Update q22.sql

--- python/scripts/timeplus/q22.sql
+++ python/scripts/timeplus/q22.sql
@@ -1,4 +1,11 @@
-CREATE STREAM bid
+drop stream if exists sink_mv;
+drop stream if exists mv;
+select sleep(3);
+drop stream if exists bid;
+drop stream if exists target;
+drop stream if exists bid_ext;
+
+CREATE STREAM bid_ext
 (
   auction  int64,
   bidder  int64,
@@ -22,6 +29,30 @@ CREATE EXTERNAL STREAM target(
     SETTINGS type='kafka', 
              brokers='kafka:9092', 
              topic='NEXMARK_Q22', 
+             data_format='JSONEachRow',
+             one_message_per_row=true;
+CREATE STREAM bid
+(
+  auction  int64,
+  bidder  int64,
+  price  int64,
+  channel  string,
+  url  string,
+  date_time  datetime64,
+  extra  string
+);
+select sleep(3);
+CREATE MATERIALIZED VIEW sink_mv INTO bid AS
+    select
+        raw:auction::int64 AS auction,
+        raw:bidder::int64 AS bidder,
+        raw:price::int64 AS price,
+        raw:channel::string AS channel,
+        raw:url::string AS url,
+        raw:date_time:datetime64 AS date_time,
+        raw:extra AS extra
+    FROM bid_ext
+    SETTINGS seek_to = 'earliest';
              data_format='JSONEachRow',
              one_message_per_row=true;
 
