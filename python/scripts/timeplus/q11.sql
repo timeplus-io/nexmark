dcf8221027001e0e2105a7172364d3382ccb10f7
kui.liu <165614821+kuiliu88@users.noreply.github.com>
2025-07-28 15:48:42 +0800

    Update q11.sql

--- python/scripts/timeplus/q11.sql
+++ python/scripts/timeplus/q11.sql
@@ -27,7 +27,26 @@ CREATE EXTERNAL STREAM target(
              topic='NEXMARK_Q11', 
              data_format='JSONEachRow',
              one_message_per_row=true;
-
+CREATE STREAM bid
+(
+  auction int64,
+  bidder int64,
+  price int64,
+  date_time datetime64,
+  extra string
+);
+select sleep(3);
+CREATE MATERIALIZED VIEW sink_mv INTO bid AS
+    select
+        raw:auction::int64 AS auction,
+        raw:bidder::int64 AS bidder,
+        raw:price::int64 AS price,
+        raw:date_time:datetime64 AS date_time,
+        raw:extra AS extra
+    FROM bid_ext
+    SETTINGS seek_to = 'earliest';
+             data_format='JSONEachRow',
+             one_message_per_row=true;
 -- User Sessions
 CREATE MATERIALIZED VIEW mv INTO target AS 
   SELECT
