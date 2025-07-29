ec2b53cfe7e16398187c375a601a2922889230f0
kui.liu <165614821+kuiliu88@users.noreply.github.com>
2025-07-29 15:51:44 +0800

    Update q19.sql

--- python/scripts/timeplus/q19.sql
+++ python/scripts/timeplus/q19.sql
@@ -14,13 +14,7 @@ drop stream if exists auction_ext;
 select sleep(3);
 CREATE STREAM bid_ext
 (
-  auction  int64,
-  bidder  int64,
-  price  int64,
-  channel  string,
-  url  string,
-  date_time  datetime64,
-  extra  string
+  raw  string
 )
 ENGINE = ExternalStream
 SETTINGS type = 'kafka', brokers = 'kafka:9092', topic = 'nexmark-bid';
