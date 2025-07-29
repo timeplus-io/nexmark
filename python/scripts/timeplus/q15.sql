b0b199934fe3742117a1ce3cdddc5c2b3ae86964
kui.liu <165614821+kuiliu88@users.noreply.github.com>
2025-07-29 13:56:38 +0800

    Update q15.sql

--- python/scripts/timeplus/q15.sql
+++ python/scripts/timeplus/q15.sql
@@ -4,6 +4,7 @@ select sleep(3);
 drop stream if exists bid;
 drop stream if exists target;
 drop stream if exists bid_ext;
+select sleep(3);
 CREATE STREAM bid_ext
 (
   auction  int64,
@@ -54,8 +55,7 @@ CREATE MATERIALIZED VIEW sink_mv INTO bid AS
         raw:extra AS extra
     FROM bid_ext
     SETTINGS seek_to = 'earliest';
-             data_format='JSONEachRow',
-             one_message_per_row=true;
+
 -- 
 CREATE MATERIALIZED VIEW mv INTO target AS 
   SELECT
